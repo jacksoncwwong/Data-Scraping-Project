46b01075041b218357017e6a14bc1c7700e05461
JacksonWong <jackson.cw.wong@gmail.com>
2015-07-12 18:02:39 -0400

    got the entries to load, along with the ranking

--- data.rb
+++ data.rb
@@ -1,15 +1,17 @@
+require "sinatra"
 require "nokogiri"
 require "open-uri"
-require "sinatra"
 
 get '/' do
 
 	url = "http://www.reddit.com/"
 	data = Nokogiri::HTML(open(url))
-	@entries = data.css('.title.may-blank')
-
-	@entries.each do |entry|
-		entry.at_css('.title').text
-	end
+	@entry = data.css('.thing')
+	erb :index
 
 end
+
+
+	# @entries.each do |entry|
+	# 	entry.at_css('.title').text
+	# end
