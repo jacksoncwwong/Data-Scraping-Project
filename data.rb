31966d2fc3e0ac49e26501b14f06645423edc722
JacksonWong <jackson.cw.wong@gmail.com>
2015-07-12 12:27:47 -0400

    uploading other relevant files

---
+++ data.rb
@@ -0,0 +1,12 @@
+require "nokogiri"
+require "open-uri"
+
+url = "http://www.reddit.com/"
+data = Nokogiri::HTML(open(url))
+@entries = data.css('.title.may-blank')
+
+@entries.each do |entry|
+
+entry.at_css('.title').text
+
+end
